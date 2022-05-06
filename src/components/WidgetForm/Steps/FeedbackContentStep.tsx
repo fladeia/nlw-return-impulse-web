c40e34e1aaff29e21145cc19cb728c39c3591e8c
fladeia <ladeiadev@gmail.com>
2022-05-06 20:48:34 -0300

    integrated with backend

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from "phosphor-react"
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
+import { api } from "../../../services/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 
@@ -13,14 +15,22 @@ interface FeedbackContentStepProps {
 
 export const FeedbackContentStep = ({ feedbackType, onFeedbackSent, onFeedbackRestartRequested }: FeedbackContentStepProps) => {
   const [screenshot, setScreenshot] = useState<string | null>(null)
-  const [comment, setComment] = useState('')
+  const [comments, setcomments] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   const handleSubmitFeedback = (e: FormEvent) => {
     e.preventDefault()
-    console.log(screenshot, comment)
+    setIsSendingFeedback(true)
 
+    api.post('/feedbacks', {
+      type: feedbackType,
+      comments,
+      screenshot,
+    })
+
+    setIsSendingFeedback(false)
     onFeedbackSent()
   }
 
@@ -41,7 +51,7 @@ export const FeedbackContentStep = ({ feedbackType, onFeedbackSent, onFeedbackRe
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
-          onChange={event => setComment(event.target.value)}
+          onChange={event => setcomments(event.target.value)}
         />
         <footer className="flex gap-2 mt-2">
           <ScreenshotButton
@@ -51,9 +61,9 @@ export const FeedbackContentStep = ({ feedbackType, onFeedbackSent, onFeedbackRe
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
-            disabled={comment.length === 0}
+            disabled={comments.length === 0 || isSendingFeedback}
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
